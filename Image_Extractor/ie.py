754e4beca8802247a1c0ac20c76322eac5134e55
devops-sagar <pathaksagar16@gmail.com>
2023-02-13 19:35:22 -0800

    bugs up

--- Image_Extractor/ie.py
+++ Image_Extractor/ie.py
@@ -1,11 +1,10 @@
-from pikepdf import Pdf, Name, PdfImage
-import os
+from pikepdf import Pdf, PdfImage
 
 file = Pdf.open("Image_Extractor/spdf.pdf")
 
 page = file.pages[0]
-# print(list(page.Images.keys()))
+print(list(page.Images.keys()))
 
-raw = page.images['/Images']
-pdf_image = PdfImage(raw)
-pdf_image.extract_to(fileprefix = "t1")
+# raw = page.images['/Images']
+# pdf_image = PdfImage(raw)
+# pdf_image.extract_to(fileprefix = "t1")
