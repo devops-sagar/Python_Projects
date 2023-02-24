e5df71ecfda3a35589f3af1a1d5f66ccb5403b50
devops-sagar <pathaksagar16@gmail.com>
2023-02-23 16:57:12 -0800

    added text to speech project

--- text-to-speech/tts.py
+++ text-to-speech/tts.py
@@ -1,9 +1,9 @@
-# import win32com.client
-# l = ['Raam', 'Krishna', 'Hanuman']
-# speaker = win32com.client.Dispatch("SAPI.SpVoice")
-# i = 0
-# while (i < 1):
-#     speaker.Speak(f"Jai Shree {l[0]}")
-#     speaker.Speak(f"Jai Shree {l[1]}")
-#     speaker.Speak(f"Jai Shree {l[2]}")
-#     i += 1
+import win32com.client
+l = ['Raam', 'Krishna', 'Hanuman']
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+i = 0
+while (i < 1):
+    speaker.Speak(f"Jai Shree {l[0]}")
+    speaker.Speak(f"Jai Shree {l[1]}")
+    speaker.Speak(f"Jai Shree {l[2]}")
+    i += 1
