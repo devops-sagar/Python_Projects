04cc69b41794b7c38faa14196ab1db4880d9eac9
devops-sagar <pathaksagar16@gmail.com>
2023-02-27 15:38:44 -0800

    Trying to develope auto send email application

---
+++ send_auto_emails/sae.py
@@ -0,0 +1,20 @@
+# xblsihtzjazlcbww
+# zrpumhmyvpwgmpxp
+
+import os
+import random
+import smtplib
+
+def auto_emails():
+    user = input("Please Enter your Name: ")
+    email = input(f"Hello {user}! Please Enter your Email ID: ")
+    message = (f"Dear {user}, Thanks for getting connected with Me!")
+
+    s = smtplib.SMTP('smtp.gmail.com:587')
+    s.ehlo()
+    s.starttls()
+    s.login("pathaksagar16@gmail.com", "zrpumhmyvpwgmpxp")
+    s.sendmail('&&&&&&&&&&&&', email, message)
+    print("Email sent successfully...!")
+
+auto_emails()
