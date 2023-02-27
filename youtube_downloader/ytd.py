11c7a0a176d9f5fdb5010db797547f5c05c36270
devops-sagar <pathaksagar16@gmail.com>
2023-02-26 18:30:34 -0800

    Jai Shree Ram

--- youtube_downloader/ytd.py
+++ youtube_downloader/ytd.py
@@ -1,5 +1,7 @@
 from pytube import YouTube
+import os
 
-YouTube('https://youtu.be/XKtiIP_-dFE').streams().first().download()
-yt = YouTube('https://www.youtube.com/watch?v=XKtiIP_-dFE')
+print(os.getcwd())
+YouTube('https://youtu.be/dva0jOeov7I').streams().first().download()
+yt = YouTube('https://www.youtube.com/watch?v=dva0jOeov7I')
 yt.streams.filter(progressive = True, file_extension = 'mp4').order_by('resolution').desc().first().download()
